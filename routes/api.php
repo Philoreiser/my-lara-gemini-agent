3db6a807ad98d38fbb931166fe63852674035a1b
Dylan Chiou <dylanchiou@Dylans-MacBook-Pro.local>
2025-06-04 18:40:17 +0800

    Make a 'QuickAsk' API endpoint with a 'hello' message

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\Api\QuickAskController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+// Quick Ask Gemini a question
+Route::post('/QuickAsk', QuickAskController::class);
